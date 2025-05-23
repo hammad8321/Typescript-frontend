ffbdf64e29cecc3c03b7988b7a5e21e32004a64f
HSRana <hsrana@brandservices.com.au>
2025-05-23 10:54:23 +1000

    update till V221

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>i am a Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>i am a Header</div>
+  )
+}
+
+export default Header
